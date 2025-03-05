f0ff118b6b530b04b2d706c8f890cf8041a7e3b7
Toby <latebloomerguy99@gmail.com>
2025-03-06 03:08:39 +0700

    updated app.js

--- app.js
+++ app.js
@@ -165,6 +165,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("Serving on Port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Serving on Port 3000");
+// });
+module.exports = app;
